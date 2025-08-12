0a393e15c11b0c64134de18ca35aed8fc8eb596f
Aman <amanprajapati22@gmail.com>
2025-08-12 23:55:55 +0530

    MongoDB demo added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Aman",
+    author_email="amanprajapati22@gmail.com",
+    packages=find_packages()
+)
